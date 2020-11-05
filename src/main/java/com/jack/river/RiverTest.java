1991ba2e0bd3ddf540c245f718969d117f7f579c
cold-river <280942553@qq.com>
2020-11-05 10:20:13 +0800

    update cold-river

--- src/main/java/com/jack/river/RiverTest.java
+++ src/main/java/com/jack/river/RiverTest.java
@@ -7,6 +7,7 @@ package com.jack.river;
  */
 public class RiverTest {
     public static void main(String[] args) {
-
+        System.out.println("----------------");
+        System.out.println("~~~~~~~~~~~~~~~~");
     }
 }
